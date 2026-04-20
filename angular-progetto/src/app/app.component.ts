9a714c79a08cfacd4d0cb4559174e965f142a721
mogaigor <moga.igor@iisgalvanimi.edu.it>
2026-04-20 10:38:05 +0000

    5

--- angular-progetto/src/app/app.component.ts
+++ angular-progetto/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserItemComponent } from './user-item/user-item.component';
 import { HelloWordComponent } from './hello-word/hello-word.component';
-
+import { UserListComponent } from './user-list/user-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserItemComponent, HelloWordComponent],
+  imports: [CommonModule, RouterOutlet, UserItemComponent, HelloWordComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
